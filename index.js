f84d5c4aaf48c1595ebf23fe20ca259b0504b29a
Ryan Mottola <rmottola68@gmail.com>
2022-06-10 12:17:49 -0400

    commit tree 228a88bafbf00bd73b2255f266f5527f1d4ad928

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+let drivers = [{name : 'tony', hometown : 'new york'}, {name : 'bobby', hometown: 'bronx'}, 'sally', 'gregory', 'stevie', 'paulie'];
+
+let findMatching = (array, string) => {
+    let newArray = array.filter( element =>{
+        return (element.toLowerCase() === string.toLowerCase())
+    })
+    return newArray
+}
+
+let fuzzyMatch = (array, letter) => {
+    let newArray = array.filter( element => {
+        return (element.slice(0, letter.length) === letter)
+    })
+    return newArray;
+}
+
+let matchName = (array, str) => {
+    let newArray = array.filter( element => {
+        return (element.name === str)
+    })
+    return newArray
+}
+
+console.log(matchName(drivers, 'bobby'))
